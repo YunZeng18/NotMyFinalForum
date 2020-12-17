7b3606841004a6fd6dc10710f971caf50a37404d
7yundao <yunzeng18@gmail.com>
2020-12-17 01:19:38 -0800

    styling

--- front/src/module/SideNav.js
+++ front/src/module/SideNav.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 export default class SideNav extends Component {
     render() {
         return (
             <nav className="side-nav">
-                <Link to="/forum/list">Browse forums</Link>
-                <Link to="/forum/create">Create a forum +</Link>
+                <NavLink className="side-nav__link" activeClassName="side-nav__link--active" to="/forum/list">Browse forums</NavLink>
+                <NavLink className="side-nav__link" activeClassName="side-nav__link--active" to="/forum/create">Create a forum +</NavLink>
                 <h2 className="side-nav__heading">My forums </h2>
                 {this.props.isAuthenticated &&
-                    <ul className="side-nav__my-forums">
+                    <ul className="side-nav__group">
                         {this.props.userForum &&
-                            this.props.userForum.map(item => <li className="side-nav__link"><Link to={`/forum/${item}`}>{item}</Link></li>)}
+                            this.props.userForum.map(item => <li ><NavLink className="side-nav__link" activeClassName="side-nav__link--active" to={`/forum/${item}`}>{item}</NavLink></li>)}
                     </ul>
                 }
-                <h2 className="side-nav__heading">Messages +</h2>
-                <ul>
-                    <li className="side-nav__link">Admin</li>
-                    <li className="side-nav__link">Activity Feed</li>
-                    <li className="side-nav__link">user2</li>
-                    <li className="side-nav__link">message another user +</li>
+                <h2 className="side-nav__heading">Messages</h2>
+                <ul className="side-nav__group">
+                    <li><NavLink className="side-nav__link" activeClassName="side-nav__link--active" to={`/message/admin`}>Admin</NavLink></li>
+                    <li><NavLink className="side-nav__link" activeClassName="side-nav__link--active" to={`/message/activity`}>Activity Feed</NavLink></li>
+
+                    <li><NavLink className="side-nav__link" activeClassName="side-nav__link--active" to={'/message/create'}>Compose message +</NavLink></li>
                 </ul>
             </nav>
         );
